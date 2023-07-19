e5220ad76bb353c3c901ed0168d7d13c304a6e43
redamjibel <redamjibel@hotmail.com>
2023-07-19 12:50:29 -0700

    headerfile

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int exec(char **argv, char *token, pid_t, int value,
+		char *input, char *delim);
