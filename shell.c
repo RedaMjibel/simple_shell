8eddeeb762bbcf62769fb5b5ef9fd7dd6643feeb
redamjibel <redamjibel@hotmail.com>
2023-07-26 07:47:19 -0700

    update

--- shell.c
+++ shell.c
@@ -28,29 +28,25 @@ int main(void)
 		exit(0);
 	}
 	printf("#cisfun$ ");
-	check = getline(&input, &n, stdin);
-	if (check == EOF)
-	{
-		free(input);
-		return (-1);
-	}
-	while (input)
+	while ((check = getline(&input, &n, stdin)) != -1)
 	{
 		input_cp = strdup(input);
 		token = strtok(input_cp, delim);
+		if (token == NULL)
+		{
+			free(input_cp);
+			printf("#cisfun$ ");
+			continue;
+		}
 		if (strcmp(token, "exit") == 0)
 		{
-			free(input_cp), free(input), exit(0);
+			free(input_cp);
+			free(input);
+			exit(0);
 		}
 		free(input_cp);
 		exec(argv, token, pid, input, delim);
 		printf("#cisfun$ ");
-		check = getline(&input, &n, stdin);
-		if (check == -1)
-		{
-			free(input);
-			exit(98);
-		}
 	}
 	free(input);
 	return (0);
